09c47954a74adb66347ae40277a3a168a69dff95
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-04-26 10:25:20 -0500

    manejo de errores

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
-const moviesAPI = require('./routes/movies');
+const moviesApi = require('./routes/movies.js');
 
+const {
+  logErrors,
+  wrapErrors,
+  errorHandler
+} = require('./utils/middleware/errorHandlers.js');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
+// body parser
 app.use(express.json());
 
-moviesAPI(app);
+// routes
+moviesApi(app);
+
+// Catch 404
+app.use(notFoundHandler);
+
+// Errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
