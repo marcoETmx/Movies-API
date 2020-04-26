2aab2f92889655c6f34834574f04c0db57633118
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-04-26 08:52:42 -0500

    implementado crud

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
+const moviesAPI = require('./routes/movies');
 
-app.get('/', function(req, res) {
-  res.send('hello world');
-});
-
-app.get('/json', function(req, res) {
-  res.json({ hello: 'world' });
-});
+moviesAPI(app);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
